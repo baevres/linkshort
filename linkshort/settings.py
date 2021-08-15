9bf939d7d88aa1b28cc6d04634e087db726a6579
baevres <88624125+baevres@users.noreply.github.com>
2021-08-15 18:39:08 +0300

    Update settings.py

--- linkshort/settings.py
+++ linkshort/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import os
 from pathlib import Path
+from pytz import timezone
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -113,7 +114,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'ru'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = timezone('Europe/Kiev')
 
 USE_I18N = True
 

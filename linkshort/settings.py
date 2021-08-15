7beb2969084421916c8aecb5440d33d6c6b135f6
baevres <88624125+baevres@users.noreply.github.com>
2021-08-15 14:35:36 +0300

    Update settings.py

--- linkshort/settings.py
+++ linkshort/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
         'NAME': 'linkshort',
         'USER': 'postgres',
         'PASSWORD': 'xxx270716',
-        'HOST': '127.0.0.1',
+        'HOST': 'linkshor.herokuapp.com',
         'PORT': '5432'
     }
 }
@@ -111,7 +111,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru'
 
 TIME_ZONE = 'UTC'
 

0aeff4341b247ced6b1cc504d42131e057413c83
baevres <88624125+baevres@users.noreply.github.com>
2021-08-24 23:35:17 +0300

    Update views.py

--- main/views.py
+++ main/views.py
@@ -1,51 +1,58 @@
 from django.shortcuts import render, redirect
 from django.db.utils import IntegrityError
+from django.utils import timezone
 
 from .models import *
 from .base import *
-from django.utils import timezone
+
 import datetime
 
 
-def create_url(in_model):
+def create_url(in_model, lst):
     """
-    Creating records in model ShortLink with using functions search() and decoder() from base.py.
-    If record exists, the first digits after letter will be increased on 1.
+    Creating records in model ShortLink with using functions search() and generator() from base.py.
+    At first main_page will use search() for checking input string and then gives lst here.
+    Infinity circle <while> will generate new string and try save it in model while it is possible and don`t call the IntegrityError.
+    If record exists, circle will be continue.
     """
-    in_model.end_time = (datetime.datetime.now() + datetime.timedelta(hours=1)).time()
-    lst = search(in_model.url)
-    dec = decoder(lst[1])
-
-    try:
-        in_model.short_url = lst[0] + dec[0]
-        in_model.save()
-    except IntegrityError:
-        dec[1] = str(int(dec[1]) + 1)
-        in_model.short_url = lst[0] + dec[0][:6]
-        in_model.save()
+    in_model.end_time = datetime.datetime.now(tz=None) + datetime.timedelta(hours=1)
+    while True:
+        try:
+            dec = generator()
+            in_model.short_url = lst[0] + next(dec)[0]
+            in_model.save()
+            break
+        except IntegrityError:
+            continue
     return
 
 
 def main_page(request):
     if request.method == 'POST':
         url = request.POST['url']
-        in_model = ShortLink.objects.get_or_create(url=url)[0]
-        if not in_model.short_url:
-            create_url(in_model)
+        lst = search(url)
+        if lst:
+            in_model = ShortLink.objects.get_or_create(url=url)[0]
+            if not in_model.short_url:
+                create_url(in_model, lst)
 
-            return redirect('url_page', in_model.id)
-        else:
-            return redirect('url_page', in_model.id)
+                return redirect('url_page', in_model.id)
+            else:
+                return redirect('url_page', in_model.id)
 
+    last_urls = ShortLink.objects.all().order_by('-end_time')
+    if len(last_urls) >= 6:
+        last_urls = last_urls[:6]
     context = {
         'title': 'LinkShort - сократит ссылку в миг',
-
+        'urls': last_urls
     }
     return render(request, 'main/main_page.html', context)
 
 
 def url_page(request, url_id):
-    now = datetime.datetime.now().time()
+    now = timezone.make_aware(datetime.datetime.now(), timezone.get_default_timezone())
+
     current_url = ShortLink.objects.get(pk=url_id)
 
     if now >= current_url.end_time:
@@ -57,8 +64,7 @@ def url_page(request, url_id):
     context = {
         'title': 'Сокращенная ссылка',
         'current_url': current_url,
-        'now': now
-        
+
     }
     return render(request, 'main/url_page.html', context)
 

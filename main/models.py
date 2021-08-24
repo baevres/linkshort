e99d6dcc5184ccf35fc68685a130bac3417308a0
baevres <88624125+baevres@users.noreply.github.com>
2021-08-24 23:34:40 +0300

    Update models.py

--- main/models.py
+++ main/models.py
@@ -4,4 +4,4 @@ from django.db import models
 class ShortLink(models.Model):
     url = models.TextField('Полученная ссылка', unique=True)
     short_url = models.CharField('Короткая ссылка', max_length=100, null=True, unique=True)
-    end_time = models.TimeField('Дедлайн ссылки', null=True)
+    end_time = models.DateTimeField('Дедлайн ссылки', null=True)

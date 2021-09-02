9e395d9b761a5c6511a1ce763ab3d8d8d806bf7f
baevres <88624125+baevres@users.noreply.github.com>
2021-09-02 22:24:34 +0300

    Update tasks.py

--- main/tasks.py
+++ main/tasks.py
@@ -8,7 +8,7 @@ from .models import *
 @app.task(bind=True)
 def delete_url(self):
     now = timezone.make_aware(datetime.datetime.now(), timezone.get_default_timezone())
-    urls = ShortLink.objects.filter(end_time__gte=now)
+    urls = ShortLink.objects.filter(end_time__lte=now)
     if urls:
         urls.delete(hour='*', minute=0)
     return

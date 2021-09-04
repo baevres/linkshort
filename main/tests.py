56afd51182d504e10fac266580109dcf2dfb8f6a
baevres <88624125+baevres@users.noreply.github.com>
2021-09-04 19:17:07 +0300

    Update tests.py

--- main/tests.py
+++ main/tests.py
@@ -1,3 +1,40 @@
 from django.test import TestCase
 
-# Create your tests here.
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from webdriver_manager.chrome import ChromeDriverManager
+
+
+test_url = 'https://webdevblog.ru/nasledovanie'
+
+
+class TestSmoke(TestCase):
+
+    def test_input(self):
+        driver = webdriver.Chrome(ChromeDriverManager().install())
+
+        driver.get('http://localhost:8000/')
+        input_url = driver.find_element_by_id('input_url')
+        submit = driver.find_element_by_id('submit_form')
+        
+        input_url.send_keys(test_url)
+        submit.send_keys(Keys.RETURN)
+
+        assert 'Сокращенная ссылка' in driver.page_source
+
+        driver.close()
+
+    def test_input_negative(self):
+        driver = webdriver.Chrome(ChromeDriverManager().install())
+
+        params = ['', 'https://webdevblog.ru/', 'fffhheaeaejaekfk']
+        for param in params:
+            driver.get('http://localhost:8000/')
+            input_url = driver.find_element_by_id('input_url')
+            submit = driver.find_element_by_id('submit_form')
+
+            input_url.send_keys(param)
+            submit.send_keys(Keys.RETURN)
+            assert 'Сокращенная ссылка' not in driver.page_source
+
+        driver.close()
